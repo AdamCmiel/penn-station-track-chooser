93f58d820069a486093ebedd833271613fb13a8f
Adam Cmiel <adamcmiel@gmail.com>
2020-05-10 11:32:53 -0400

    Initial Commit

---
+++ Penn Station Track ChooserTests/Penn_Station_Track_ChooserTests.swift
@@ -0,0 +1,34 @@
+//
+//  Penn_Station_Track_ChooserTests.swift
+//  Penn Station Track ChooserTests
+//
+//  Created by Adam Cmiel on 5/10/20.
+//  Copyright © 2020 Adam Cmiel. All rights reserved.
+//
+
+import XCTest
+@testable import Penn_Station_Track_Chooser
+
+class Penn_Station_Track_ChooserTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
